65c9e74e8d83822f1b647004aa813bea69098532
Julian Florentin <137744274+FlorentinJulian@users.noreply.github.com>
2024-03-04 17:36:02 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-const App = () => {
+import Aside from "./Componentes/Aside";
+import ModalPokemon from "./Componentes/ModalPokemon";
+import Pokemons from "./Componentes/Pokedex";
+import usePokemonContext from "./hooks/usePokemonContext";
+
+function App() {
+  const { showDetailPokemon, closePokemonDetail, pokemonDetail, isLoading } =
+    usePokemonContext();
+
   return (
-    <>
-      <h1 className="text-5xl font-bold">Pokedex</h1>
-    </>
+    <section className="bg-[#F6F8FC] h-screen font-outfit overflow-y-auto bg-[url(/pokeball-icon.png)] bg-no-repeat bg-[-10%_-20%] overflow-x-hidden">
+      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]">
+        <Pokemons />
+        <Aside pokemon={pokemonDetail} isLoading={isLoading} />
+        <ModalPokemon
+          showModal={showDetailPokemon}
+          onCloseModal={closePokemonDetail}
+          pokemon={pokemonDetail}
+        />
+      </main>
+    </section>
   );
-};
+}
 
 export default App;
---
+++ src/hooks/UsePokemonContext.js
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { PokemonContext } from "../Context/PokemonContext";
+
+const UsePokemonContext = () => useContext(PokemonContext);
+
+export default UsePokemonContext;
